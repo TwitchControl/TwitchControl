da6a4628633cb6d97c9370475c1ac96754b93bb7
Nayla Hanegan <naylahanegan@gmail.com>
2024-11-04 22:14:06 -0500

    der

--- build-win32.bat
+++ build-win32.bat
@@ -1,19 +1,2 @@
 @echo off
-rmdir dist /s /q
-
-REM Get the short git hash
-FOR /F "tokens=*" %%i IN ('git rev-parse --short HEAD') DO SET GIT_HASH=%%i
-
-REM Update version.py with the git hash
-echo def getVersion(): > version.py
-echo     return "%%GIT_HASH%%" >> version.py
-
-pyinstaller --onefile .\main.py --name="Twitch Control" -w && 
-mkdir dist\plugins
-
-for /R plugins %%f in (*.json5) do (
-    set "relPath=%%f"
-    call set "relPath=%%relPath:plugins\=%%"
-    call mkdir "dist\plugins\%%~dprelPath%%"
-    copy "%%f" "dist\plugins\%%~dprelPath%%"
-)
+pyinstaller --onefile .\main.py --name="Twitch Control" -w
